db2982d9c964f4d256785738f64262f9856d41d8
programKingdom <programKingdom@163.com>
2019-05-06 09:49:23 +0800

    news public_address

--- public_address/public/index.php
+++ public_address/public/index.php
@@ -2,23 +2,16 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-namespace think\config\driver;
+// [ 应用入口文件 ]
 
-class Json
-{
-    public function parse($config)
-    {
-        if (is_file($config)) {
-            $config = file_get_contents($config);
-        }
-        $result = json_decode($config, true);
-        return $result;
-    }
-}
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../application/');
+// 加载框架引导文件
+require __DIR__ . '/../thinkphp/start.php';
